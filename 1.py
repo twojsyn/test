e60a92779545919a7dfcedd92eab6e773149fc15
twojsyn <106533346+twojsyn@users.noreply.github.com>
2022-12-13 19:26:51 +0100

    label i frame

--- 1.py
+++ 1.py
@@ -1,11 +1,25 @@
 from tkinter import *
+from tkinter import filedialog as fd
 
+
+def add_image():
+    filetypes = (
+        ('obrazy PNG', '*.png'),
+        ('All files','*.*')
+    )
+    fd.askopenfilename(filetypes=filetypes)
 
 window = Tk()
 window.minsize(width=640, height=480)
 window.maxsize(width=640, height=480)
 
 photo = PhotoImage(file="123.png")
+
+frame1 = Frame(window, bg="black", height=50)
+frame1.pack()
+
+addButton = Button(frame1, command=add_image, text = "Dodaj obrazek", font = ("Arial, 24"))
+addButton.pack()
 
 mainFrame = Label(window,
                   text="Moja pierwsza gra",
