99e7b69d3c24820d942acc7d436e7f36f494a269
twojsyn <106533346+twojsyn@users.noreply.github.com>
2022-12-13 18:50:21 +0100

    label i frame

--- 1.py
+++ 1.py
@@ -1,10 +1,16 @@
 from tkinter import *
 
 
-
 window = Tk()
-
 window.minsize(width=640, height=480)
 window.maxsize(width=640, height=480)
+
+label1 = Frame(window, width = 600, height = 400, bd=3, bg="black")
+label2 = Label(window, text="Jakiś tekst", bd=3, bg="blue" , font = ("Arial", 24), fg="white")
+
+label1.pack()
+label2.pack()
+
+
 
 window.mainloop()
